1be72270c5d2a3e3259d0ba3635add25e4fc09cb
BestTTTA <bst12546@gmail.com>
2024-06-12 11:42:30 +0700

    fix error

--- src/components/RoleCategory.tsx
+++ src/components/RoleCategory.tsx
@@ -2,13 +2,13 @@ import GetRole from "@/services/api/GetRole";
 
 
 const RoleCategory = ({ roleFrameID }: { roleFrameID: number }) => {
-    const { roleID, isLoading, error } = GetRole(roleFrameID);
+    const { roleName, isLoading, error } = GetRole(roleFrameID);
   
     if (isLoading) return null;
     if (error) return null;
   
     return (
-      <div className="flex text-[#03FCA9]">AI Type:<p className="px-2 text-white">{roleID}</p></div>
+      <div className="flex text-[#03FCA9]">AI Type:<p className="px-2 text-white">{roleName}</p></div>
     );
   };
   
