271c08ee275a2e51a31a196083234ae157838ce0
BestTTTA <bst12546@gmail.com>
2024-06-17 20:56:36 +0700

    update

--- src/components/ButtonChangeLanguage.tsx
+++ src/components/ButtonChangeLanguage.tsx
@@ -8,12 +8,6 @@ export default function ButtonChangeLanguage() {
     const [isOpen, setIsOpen] = useState(false);
     const [selectedLocale, setSelectedLocale] = useState(locale || 'th');
 
-    {/* -------------------delete this when up to production 😈------------------- */ }
-    const [prank, setPrank] = useState(false)
-    const [en, setEn] = useState(false)
-    const [sn, setSn] = useState(false)
-    {/* -------------------delete this when up to production 😈------------------- */ }
-
     const handleChangeLanguage = (newLocale: string) => {
         setSelectedLocale(newLocale);
         router.push(router.pathname, router.asPath, { locale: newLocale });
@@ -64,52 +58,11 @@ export default function ButtonChangeLanguage() {
                             English
                             <Image src="/png/united-states-of-america.png" alt="Thailand flag" width={20} height={20} className="ml-2" />
                         </li>
-                        <li
-                            onClick={() => setPrank(prev => !prev)}
-                            className="cursor-pointer flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                        >
-                            Spanish
-                            <Image src="/png/spanish.png" alt="Thailand flag" width={20} height={20} className="ml-2" />
-                        </li>
                     </ul>
                 </div>
             )}
 
-            {/* -------------------delete this when up to production 55555------------------- */}
-            {prank && (
-                <div className="fixed inset-0 flex border justify-center items-center">
-                    <div className="flex p-4 items-center justify-center rounded-md gap-2 bg-white drop-shadow-lg ">
-                        <button onClick={() => { setPrank(false) }} className="hover:scale-105 hover:text-red-500">Close</button>
-                        <div className="flex border gap-2 rounded-md shadow-lg p-1 hover:scale-105">
-                            <button onClick={() => { setEn(true) }}>English</button>
-                            <Image src="/png/united-states-of-america.png" alt="Thailand flag" width={24} height={20} />
-                        </div>
-                        <strong>or</strong>
-                        <div className="flex border gap-2 rounded-md shadow-lg p-1 hover:scale-105">
-                            <button onClick={() => { setSn(true) }}>Spanish</button>
-                            <Image src="/png/spanish.png" alt="Thailand flag" width={24} height={20} />
-                        </div>
-                    </div>
-                </div>
-            )}
-
-            {en && (
-                <div className="fixed inset-0 flex border justify-center items-center">
-                    <div className="flex p-8 items-center justify-center rounded-md gap-2 bg-white drop-shadow-lg ">
-                        <button onClick={() => { setEn(false), setPrank(false) }} className="hover:scale-105 hover:text-red-500">Close</button>
-                        <h1 className="font-bold">Who ever move first is gay.</h1>
-                    </div>
-                </div>
-            )}
-            {sn && (
-                <div className="fixed inset-0 flex border justify-center items-center">
-                    <div className="flex p-8 items-center justify-center rounded-md gap-2 bg-white drop-shadow-lg ">
-                        <button onClick={() => { setSn(false), setPrank(false) }} className="hover:scale-105 hover:text-red-500">Close</button>
-                        <h1 className="font-bold">Quien se mueve primero es gay.</h1>
-                    </div>
-                </div>
-            )}
-            {/* -------------------delete this when up to production 55555------------------- */}
+            
         </div>
     );
 }
