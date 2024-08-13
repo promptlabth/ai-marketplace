d075fe4f7fd9266a30aae0834011922db0b323ab
Kami0rn <tew965@gmail.com>
2024-08-13 16:52:24 +0700

    Replaced text and icons with images in CarouselMarketplace component

--- src/components/CorouselMarketplace.tsx
+++ src/components/CorouselMarketplace.tsx
@@ -17,12 +17,19 @@ const CarouselMarketplace: React.FC = () => {
     return (
         <Carousel {...carouselProps}>
             <div className='w-full h-full flex justify-center items-center'>
-                <h1 className='flex items-center justify-center h-full text-[25px] font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-red-700 to-orange-500'>{t('components.carouselmarketplace.lawyer')}</h1>
-                <Image src="/png/hammer.png" alt='' width={40} height={40}></Image>
+                <Image src="/png/carousel/2.png" alt='' width={600} height={284}></Image>
             </div>
             <div className='w-full h-full flex justify-center items-center'>
-                <h1 className='flex items-center justify-center h-full text-[25px] font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-blue-700 to-cyan-500'>{t('components.carouselmarketplace.scientist')}</h1>
-                <MdScience color='white' size={40} />
+                <Image src="/png/carousel/3.png" alt='' width={600} height={284}></Image>
+            </div>
+            <div className='w-full h-full flex justify-center items-center'>
+                <Image src="/png/carousel/6.png" alt='' width={600} height={284}></Image>
+            </div>
+            <div className='w-full h-full flex justify-center items-center'>
+                <Image src="/png/carousel/5.png" alt='' width={600} height={284}></Image>
+            </div>
+            <div className='w-full h-full flex justify-center items-center'>
+                <Image src="/png/carousel/4.png" alt='' width={600} height={284}></Image>
             </div>
             <div className='flex flex-col justify-center items-center h-full w-full'>
                 <button className='flex items-center justify-center h-full w-full underline text-xl text-white hover:scale-105 focus:scale-105'>{t('components.carouselmarketplace.try')}</button>
