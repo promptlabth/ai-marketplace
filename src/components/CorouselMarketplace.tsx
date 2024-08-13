0b6b3511073e8e99291d57b1d9e478eab07403e6
Kami0rn <tew965@gmail.com>
2024-08-13 16:59:18 +0700

    "Updated image widths and heights in CarouselMarketplace component"

--- src/components/CorouselMarketplace.tsx
+++ src/components/CorouselMarketplace.tsx
@@ -17,19 +17,19 @@ const CarouselMarketplace: React.FC = () => {
     return (
         <Carousel {...carouselProps}>
             <div className='w-full h-full flex justify-center items-center'>
-                <Image src="/png/carousel/2.png" alt='' width={600} height={284}></Image>
+                <Image src="/png/carousel/2.png" alt='' width={1875} height={888}></Image>
             </div>
             <div className='w-full h-full flex justify-center items-center'>
-                <Image src="/png/carousel/3.png" alt='' width={600} height={284}></Image>
+                <Image src="/png/carousel/3.png" alt='' width={1875} height={888}></Image>
             </div>
             <div className='w-full h-full flex justify-center items-center'>
-                <Image src="/png/carousel/6.png" alt='' width={600} height={284}></Image>
+                <Image src="/png/carousel/6.png" alt='' width={1875} height={888}></Image>
             </div>
             <div className='w-full h-full flex justify-center items-center'>
-                <Image src="/png/carousel/5.png" alt='' width={600} height={284}></Image>
+                <Image src="/png/carousel/5.png" alt='' width={1875} height={888}></Image>
             </div>
             <div className='w-full h-full flex justify-center items-center'>
-                <Image src="/png/carousel/4.png" alt='' width={600} height={284}></Image>
+                <Image src="/png/carousel/4.png" alt='' width={1875} height={888}></Image>
             </div>
             <div className='flex flex-col justify-center items-center h-full w-full'>
                 <button className='flex items-center justify-center h-full w-full underline text-xl text-white hover:scale-105 focus:scale-105'>{t('components.carouselmarketplace.try')}</button>
