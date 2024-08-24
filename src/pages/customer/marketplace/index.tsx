233d9af9b1482d0a6da2bd858293c1c83b016f12
Tanachod Sakthamjaroen <Tanachod.s@arise.tech>
2024-08-25 00:31:00 +0700

    add bug

--- src/pages/customer/marketplace/index.tsx
+++ src/pages/customer/marketplace/index.tsx
@@ -113,7 +113,7 @@ const CreateAgent = () => {
           {Object.keys(groupedAgents).map((roleFrameID: any) => (
             <div
               key={roleFrameID}
-              ref={(el) => (categoryRefs.current[roleFrameID] = el)}
+              ref={categoryRefs.current[roleFrameID]}
               className="flex flex-col sm:items-start gap-4 mb-4 w-full snap-x snap-mandatory hide-scrollbar overflow-x-scroll space-x-4"
             >
               <RoleCategory roleFrameID={roleFrameID} agents={groupedAgents[roleFrameID]} />
