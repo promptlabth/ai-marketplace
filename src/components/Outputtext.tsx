01c06e2be659ae23986a2983d8fe0cc2bba3ae98
Bhuwadol <125109678+BHU23@users.noreply.github.com>
2024-08-27 20:20:16 +0700

    add whiteSpace in text genarate repone

--- src/components/Outputtext.tsx
+++ src/components/Outputtext.tsx
@@ -7,13 +7,15 @@ interface Outputtype {
 
 const Outputtext: React.FC<Outputtype> = ({ content, generate }) => {
     return (
-        <div className='flex flex-col grow'>
-            <p className='text-white'>{content}</p>
-            <div className='flex-1 rounded-lg bg-[#3D434A] p-4'>
-                <p className='text-white text-lg'>{generate}</p>
-            </div>
+      <div className="flex flex-col grow">
+        <p className="text-white">{content}</p>
+        <div className="flex-1 rounded-lg bg-[#3D434A] p-4">
+          <p className="text-white text-lg" style={{ whiteSpace: "pre-wrap" }}>
+            {generate}
+          </p>
         </div>
-    )
+      </div>
+    );
 }
 
 export default Outputtext;
