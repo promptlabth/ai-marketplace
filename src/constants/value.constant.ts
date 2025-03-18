2f407e0ddf300f0eb62472e2a728d494694602ae
SiraphopSS <boomth8@gmail.com>
2025-03-18 20:43:08 +0700

    revert Testing Stripe

--- src/constants/value.constant.ts
+++ src/constants/value.constant.ts
@@ -2,7 +2,9 @@ export const subscriptionPlanPrizeIdMap: {
     [key: string]: { planId: number;  title: string; prizeId: string, price: number };
   } = {
     FREE: { planId: 1, title: "FREE", prizeId: "", price: 0 },
-    BRONZE: { planId: 2, title: "BRONZE", prizeId: "price_1R3oaMAom1IgIvKKM3O1LlgH", price: 59 },
+    BRONZE: { planId: 2, title: "BRONZE", prizeId: "price_1OLSgkAom1IgIvKK9c3qAqMT", price: 59 },
+    // Production : price_1OLSgkAom1IgIvKK9c3qAqMT
+    // Test Bronze : price_1R3oaMAom1IgIvKKM3O1LlgH
     SILVER: { planId: 3, title: "SILVER", prizeId: "price_1OLShKAom1IgIvKKt8q5fCBI", price: 199 },
     GOLD: { planId: 4, title: "GOLD", prizeId: "price_1OLSi0Aom1IgIvKKD24dXvtu", price: 299 },
     BRONZE_MONTHLY: { planId: 2, title: "MONTHLY BRONZE", prizeId: "price_1PaHGZAom1IgIvKKyKmus1gZ", price: 599 },
